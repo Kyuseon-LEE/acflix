22850f181ac131091d8acf76d12ca0b808a4a21c
kyu7470 <kyu7470@gmail.com>
2024-07-11 17:37:12 +0900

    css update2

--- src/acflix/MainHeader.jsx
+++ src/acflix/MainHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const MainHeader = () => {
   return(
-    <div class="header">
+    <div className="header">
             <a href="#none"><img src={process.env.PUBLIC_URL + '/imgs/logo.png'} alt="" /></a>
             <ul>
             <li><a href="#none">내프로필</a></li>
