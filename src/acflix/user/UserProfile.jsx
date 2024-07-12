abe9bcc16d2f9ef1aa95c95815b12f5fe22cb873
wkdwlsdjs <wkdwlsdjs@gmail.com>
2024-07-12 14:30:25 +0900

    not yet

--- src/acflix/user/UserProfile.jsx
+++ src/acflix/user/UserProfile.jsx
@@ -9,6 +9,12 @@ import '../css/index.css';
 
 const UserProfile = ({isSignIned}) => {
 
+  if (!isSignIned) {
+    // 로그인 되지 않았을 경우, '/login' 경로로 리디렉션
+    window.location.href = '/login';
+    return;
+  }
+
   // Hook
   const [myFav, setMyFav] = useState([]);
 
